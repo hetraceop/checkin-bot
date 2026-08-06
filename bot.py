b19baf8fd4b353c1913de82a7f95968f78068c27
hetraceop <hetraceop@gmail.com>
2026-08-07 01:58:34 +0600

    Update TOKEN to a hardcoded value
    Replaced environment variable TOKEN with a hardcoded value.

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from datetime import datetime
 import json
 import os
 
-TOKEN = os.getenv("TOKEN")
+TOKEN = os.getenv("8913069353:AAGUFkwYClgEOXZPc6VDgaX3YK4UTZLGVTU")
 
 DATA_FILE = "checkin_data.json"
 user_data = {}
